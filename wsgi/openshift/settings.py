d3d9d4a6d4af7169b100d2cf905a378f96bc8f45
denis_gordeev <dnsgrdv@gmail.com>
2014-09-05 02:01:25 +0400

    modified:   wsgi/openshift/settings.py

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -20,7 +20,7 @@ if os.environ.has_key('OPENSHIFT_DB_PORT'):
 
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 if ON_OPENSHIFT:
-    DEBUG = bool(os.environ.get('DEBUG', False))
+    DEBUG = True
     if DEBUG:
         print("WARNING: The DEBUG environment is set to True.")
 else:
