467b4123f286d13c199e110311fec09c89f65fed
denis_gordeev <dnsgrdv@gmail.com>
2014-09-05 01:53:32 +0400

    modified:   wsgi/openshift/settings.py

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -39,10 +39,10 @@ if ON_OPENSHIFT:
         'default': {
 'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'), # Or path to database file if using sqlite3.
-'USER': DB_USER, # Not used with sqlite3.
-'PASSWORD': DB_PASSWD, # Not used with sqlite3.
-'HOST': DB_HOST, # Set to empty string for localhost. Not used with sqlite3.
-'PORT': DB_PORT, # Set to empty string for default. Not used with sqlite3.
+'USER': '', # Not used with sqlite3.
+'PASSWORD': '', # Not used with sqlite3.
+'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+'PORT': '', # Set to empty string for default. Not used with sqlite3.
         }
     }
 else:
