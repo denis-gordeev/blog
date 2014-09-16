d99b1d531390bdd638583a09b981c9c1d5a43cd3
denis_gordeev <dnsgrdv@gmail.com>
2014-09-17 03:05:58 +0400

    modified:   wsgi/openshift/urls.py

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -14,10 +14,3 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^chat/$', chat, name='chat'),
 )
-
-
-urlpatterns += patterns('', (
-    r'^static/(?P<path>.*)$',
-    'django.views.static.serve',
-    {'document_root': settings.STATIC_ROOT}
-))
